3a69710a18f77b2e05eaa2f338cde053c11a6db0
martingomez092 <martingomez092@gmail.com>
2024-02-06 15:56:09 -0300

    Dancers select

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,31 @@ const route = 'http://127.0.0.1:5500/scripts/base.json'
    fetch(route)
       .then(response => response.json())
       .then(data => {
-         const groupsSel = document.getElementById("groups");
-        groupsSel.classList.add('form-select')
+         const selectGroup = document.getElementById('selectGroup');
+         const selectDancers = document.getElementById('selectDancers')
+         selectGroup.classList.add('form-select');
+         selectDancers.classList.add('form-select')
+
          data.groups.forEach(group => {
             const option = document.createElement('option');
-            option.value = group.id; 
+            option.value = group.idGroup; 
             option.text = group.name; 
-            groupsSel.appendChild(option);
+            selectGroup.appendChild(option);        
          });
+
+         selectGroup.addEventListener('change', () => {
+            const selectedGroupId = parseInt(selectGroup.value);
+            const selectedGroup = data.groups.find(grupo => grupo.idGroup === selectedGroupId);
+            selectDancers.innerHTML = '<option value="">Selecciona un bailarín</option>';
+            selectedGroup.dancers.forEach(dancer => {
+              const option = document.createElement('option');
+              option.value = dancer.id;
+              option.textContent = dancer.name;
+              selectDancers.appendChild(option);
+            });
+
+         })
+
       })
       .catch(error => console.error('Error al cargar el archivo JSON', error));
 
