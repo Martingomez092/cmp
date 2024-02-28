7f9b402d89625a74227ff4e982aa9b8ca853cb04
martin gomez <mgomez@syncromind.com>
2024-02-28 17:56:55 -0300

    Updates table

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ fetch(route)
         (grupo) => grupo.idGroup === selectedGroupId
       );
       selectDancers.innerHTML =
-        '<option value="">Selecciona un bailarín</option>';
+        '<option value="">Selecciona una bailarina</option>';
       selectedGroup.dancers.forEach((dancer) => {
         const option = document.createElement("option");
         option.value = dancer.id;
@@ -77,26 +77,64 @@ fetch(route)
         const tableContainer = document.getElementById('payment-status-table');
         const table = document.createElement('table');
         table.classList.add('table')
+        table.classList.add('table-striped')
         const thead = document.createElement('thead');
-        const tr = document.createElement('tr');
-        const headers = ['Fecha', 'Mes', 'Tipo', 'Monto']
+        const trHead = document.createElement('tr');
+        const tbody = document.createElement('tbody');
+
+    
+        const headers = ['Fecha', 'Mes abonado', 'Valor cuota', 'Pagó', 'Saldo']
         headers.forEach( header => {
           const th = document.createElement('th')
           th.textContent = header;
-          tr.appendChild(th);
+          header == 'Valor cuota' ? th.classList.add('text-end') : "";
+          header == 'Pagó' ? th.classList.add('text-end') : "";
+          header == 'Saldo' ? th.classList.add('text-end') : "";
+          trHead.appendChild(th);
         })
-        thead.appendChild(tr);
-        table.appendChild(thead);
-        tableContainer.appendChild(table);  
+
+        const rows = dancer.payments;
+        rows.forEach( (row, index) => {
+          const trBody = document.createElement('tr');
+          const tdDate = document.createElement('td');
+          const tdMonth = document.createElement('td');
+          const tdMonthValue = document.createElement('td');
+          const tdPayment = document.createElement('td');
+          const tdBalance = document.createElement('td');
+
+          tdMonthValue.classList.add('text-end');
+          tdPayment.classList.add('text-end');
+          tdBalance.classList.add('text-end');
+
+          tdDate.textContent = dancer.payments[index].date;
+          tdMonth.textContent = dancer.payments[index].month;
+          tdMonthValue.textContent = dancer.payments[index].monthValue;
+          tdPayment.textContent = dancer.payments[index].payment;  
+          tdBalance.textContent = tdMonthValue.textContent - tdPayment.textContent;
+          tdBalance.textContent !== "0" ? tdBalance.style.color = "red" : tdBalance.style.color = "green";
+
         
+          trBody.appendChild(tdDate);
+          trBody.appendChild(tdMonth);
+          trBody.appendChild(tdMonthValue);
+          trBody.appendChild(tdPayment);
+          trBody.appendChild(tdBalance);
+          tbody.appendChild(trBody);
+        } )
+ 
+       
+        thead.appendChild(trHead);
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        tableContainer.appendChild(table);    
       }
 
       // MUESTRO ALERT DE ACUERDO AL ESTADO DE LA DEUDA
       if (dancer.paymentStatus === true) {
-        alertContainer = createAlert(`Cuota al día`, `success`);
+        alertContainer = createAlert(`Cuenta al día`, `success`);
 
       } else {
-        alertContainer = createAlert(`Cuota pendiente de pago`, `danger`);
+        alertContainer = createAlert(`Saldo pendiente de pago`, `danger`);
       }
 
       // MUESTRO TABLA
